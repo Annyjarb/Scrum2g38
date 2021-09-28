f96b3a13180915e5dfe930346bf6237058cdd2a4
Annyjarb <annyjarb@gmail.com>
2021-09-27 20:36:57 -0500

    tablas
    contiene proveedores

--- BO/creacion de tablas.sql
+++ BO/creacion de tablas.sql
@@ -17,3 +17,14 @@ INSERT INTO usuarios VALUES(1022,'@ubosque','Diego Rodriguez','admin123456','adm
 INSERT INTO usuarios VALUES(1023,'@ubosquedd','Diego Rodriguez','pass','user');
 INSERT INTO clientes VALUES(1022,'calle falsa','Diego Rodriguez','4654656','@cliente1');
 INSERT INTO clientes VALUES(1023,'calle falsa2','Diego Rodriguez','65656','@cliente2');
+
+
+CREATE TABLE proveedores(
+nit BIGINT PRIMARY KEY,
+nombre_proveedor VARCHAR(255) NOT NULL,
+direccion_proveedor VARCHAR(255) NOT NULL,
+telefono_proveedor VARCHAR(255)NOT NULL,
+ciudad VARCHAR(255) NOT NULL);
+
+INSERT INTO proveedores VALUES(1022,'ecsito','calle1','4654656','bogota');
+INSERT INTO proveedores VALUES(1023,'gumbo','calle2','65656','medellin');
